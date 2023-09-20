bbfce31508ecdf8f5b1ebf01e884b1896c51c1b4
denyskarakycha <denys.karakycha@gmail.com>
2023-09-20 16:44:40 +0300

    Added token for login user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -38,8 +39,31 @@ exports.signup = (req, res, next) => {
 exports.login = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
+    let loadedUser;
     User.findOne({email: email})
-        .then()
+        .then(user => {
+            if (!user) {
+                const error = new Error('A user not be found.');
+                error.statusCode = 401;
+                throw error;
+            }
+            loadedUser = user;
+            return bcrypt.compare(user.password, password)
+        })
+        .then(isEqual => {
+            if (!isEqual) {
+                const error = new Error('Wrong password.');
+                error.statusCode = 401;
+                throw error;
+            }
+
+            const token = jwt.sign({
+                email: loadedUser.email,
+                userId: loadedUser._id.toString()
+            }, 'secret', {expiresIn: '1h'});
+
+            res.status(200).json({token: token, userId: loadedUser._id.toString()});
+        })
         .catch((err) => {
             if (!err.statusCode) {
               err.statusCode = 500;
